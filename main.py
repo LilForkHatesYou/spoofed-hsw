0ace7977ed39521481e40236bea14fc8fefe3452
Lil Fork <lilforkhatesdiscord@gmail.com>
2023-03-31 22:08:36 -0400

    Ra
    RR

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import tls_client, json, websocket
-import random, base64, threading
-import re
+import tls_client, json, websocket, random, base64, threading, re, os, time, requests
 from colorama import Fore, Style
 from datetime import timedelta
-from libs.eazyui import *
 from libs.solver import Solver, Browser
 
 config = json.loads(open('config.json', 'r').read())
@@ -14,99 +11,39 @@ for proxy in proxies:
     proxy = proxy.rstrip()
     ip, port, user, password = proxy.split(':')
     proxy_pool.append(f'{user}:{password}@{ip}:{port}')
-
 threadList, browserList = [], []
-
 locked, unlocked, total = 0, 0, 0
-
 def updateTitle():
-    global total
-    global locked
-    global unlocked
     genStartedAs = time.time()
     while True:
         try:
-            delta = timedelta(seconds=round(time.time()-genStartedAs))
-            result = ""
-            if delta.days > 0:
-                result += f"{delta.days}d "
-            if delta.seconds // 3600 > 0:
-                result += f"{delta.seconds // 3600}h "
-            if delta.seconds // 60 % 60 > 0:
-                result += f"{delta.seconds // 60 % 60}m "
-            if delta.seconds % 60 > 0 or result == "":
-                result += f"{delta.seconds % 60}s"
-            os.system(f'title UNLOCKED: {unlocked} │ LOCKED: {locked} │ Rate: {round(unlocked/total*100,2)}% │ UPM: {round(unlocked / ((time.time() - genStartedAs) / 60))} │ Elapsed: {result}')
+            delta = timedelta(seconds=round(time.time() - genStartedAs))
+            elapsed = str(delta).split(".")[0]
+            unlocked_rate = round(unlocked / total * 100, 2)
+            upm = round(unlocked / ((time.time() - genStartedAs) / 60))
+            os.system(f'title UNLOCKED: {unlocked} │ LOCKED: {locked} │ Rate: {unlocked_rate}% │ UPM: {upm} │ Elapsed: {elapsed}')
         except Exception:
             pass
         time.sleep(1)
-
-class Output:
-    def error(txt: str) -> None:
-        Console.printError(txt, PrintType.CLEAN)
-
-    def debug(txt: str) -> None:
-        Console.printInfo(txt, PrintType.CLEAN)
-
-    def good(txt: str) -> None:
-        Console.printSuccess(txt, PrintType.CLEAN)
-
-    def other(txt: str) -> None:
-        Console.printOther(txt, PrintType.CLEAN)
-
 class Discord():
     def __init__(self) -> None:
         global total
         global locked
         global unlocked
-        #Tls Spoofing Shit
         self.session = tls_client.Session(
             client_identifier="chrome_111",
             random_tls_extension_order=True,
-            h2_settings={
-                "HEADER_TABLE_SIZE": 65536,
-                "MAX_CONCURRENT_STREAMS": 1000,
-                "INITIAL_WINDOW_SIZE": 6291456,
-                "MAX_HEADER_LIST_SIZE": 262144
-            },
-            h2_settings_order=[
-                "HEADER_TABLE_SIZE",
-                "MAX_CONCURRENT_STREAMS",
-                "INITIAL_WINDOW_SIZE",
-                "MAX_HEADER_LIST_SIZE"
-            ],
-            supported_signature_algorithms=[
-                "ECDSAWithP256AndSHA256",
-                "PSSWithSHA256",
-                "PKCS1WithSHA256",
-                "ECDSAWithP384AndSHA384",
-                "PSSWithSHA384",
-                "PKCS1WithSHA384",
-                "PSSWithSHA512",
-                "PKCS1WithSHA512",
-            ],
+            h2_settings={"HEADER_TABLE_SIZE": 65536,"MAX_CONCURRENT_STREAMS": 1000,"INITIAL_WINDOW_SIZE": 6291456,"MAX_HEADER_LIST_SIZE": 262144},
+            h2_settings_order=["HEADER_TABLE_SIZE","MAX_CONCURRENT_STREAMS","INITIAL_WINDOW_SIZE","MAX_HEADER_LIST_SIZE"],
+            supported_signature_algorithms=["ECDSAWithP256AndSHA256","PSSWithSHA256","PKCS1WithSHA256","ECDSAWithP384AndSHA384","PSSWithSHA384","PKCS1WithSHA384","PSSWithSHA512","PKCS1WithSHA512",],
             supported_versions=["GREASE", "1.3", "1.2"],
             key_share_curves=["GREASE", "X25519"],
             cert_compression_algo="brotli",
-            pseudo_header_order=[
-                ":method",
-                ":authority",
-                ":scheme",
-                ":path"
-            ],
+            pseudo_header_order=[":method",":authority",":scheme",":path"],
             connection_flow=15663105,
-            header_order=[
-                "accept",
-                "user-agent",
-                "accept-encoding",
-                "accept-language"
-            ]
-        )
+            header_order=["accept","user-agent","accept-encoding","accept-language"])
         self.proxy = "http://" + random.choice(proxy_pool)
-        self.session.proxies = {
-            "http": self.proxy,
-            "https": self.proxy
-        }
+        self.session.proxies = {"http": self.proxy,"https": self.proxy}
         self.prop = {
             "os": "Windows",
             "browser": "Chrome",
@@ -126,7 +63,7 @@ class Discord():
         }
         self.super = base64.b64encode(json.dumps(self.prop, separators=(',', ':')).encode()).decode()
 
-    def friend_request(self, token: str, id: str = '203827571299713025') -> None:
+    def friend_request(self, token: str, id: str = '422335560460271623') -> None:
         self.session.put(f'https://discord.com/api/v9/users/@me/relationships/{id}', headers={
             "origin": "https://discord.com",
             "referer": f"https://discord.com/channels/@me/{id}",
@@ -161,9 +98,7 @@ class Discord():
     def getFingerprint(self) -> str:
         response = self.session.get('https://discord.com/api/v9/experiments')
         self.session.cookies.update(response.cookies)
-        self.session.cookies.update({
-            "locale": "fr"
-        })
+        self.session.cookies.update({"locale": "fr"})
         return response.json()['fingerprint']
     
     def createAccount(self, captchaKey:str, fingerprint: str) -> str:
@@ -187,19 +122,16 @@ class Discord():
             'x-fingerprint': fingerprint,
             'x-track': 'eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6ImZyLUJFIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzExMS4wLjAuMCBTYWZhcmkvNTM3LjM2IiwiYnJvd3Nlcl92ZXJzaW9uIjoiMTExLjAuMC4wIiwib3NfdmVyc2lvbiI6IjEwIiwicmVmZXJyZXIiOiIiLCJyZWZlcnJpbmdfZG9tYWluIjoiIiwicmVmZXJyZXJfY3VycmVudCI6IiIsInJlZmVycmluZ19kb21haW5fY3VycmVudCI6IiIsInJlbGVhc2VfY2hhbm5lbCI6InN0YWJsZSIsImNsaWVudF9idWlsZF9udW1iZXIiOjk5OTksImNsaWVudF9ldmVudF9zb3VyY2UiOm51bGx9',
         }
-
-        response = self.session.post(
-            'https://discord.com/api/v9/auth/register',
+        response = self.session.post('https://discord.com/api/v9/auth/register',
             json={
                 'consent': True,
                 'fingerprint': fingerprint,
                 'username': random.choice(names),
-                'captcha_key': captchaKey,
-                #'invite': 'bNsUQY6D'
+                'captcha_key': captchaKey
             }
         )
         if not 'token' in response.json():
-            Output.debug('Rate Limited')
+            print(f"({Fore.RED}-{Style.RESET_ALL}) - Rate Limited")
             return None
         return response.json()['token']
 
@@ -210,12 +142,10 @@ class Discord():
         global total
         global locked
         global unlocked
-        #Creating the account
         solver = Solver(siteUrl='discord.com', siteKey='4c672d35-0701-42b2-88c3-78380b0db560', browser=random.choice(browserList),session=self.session)
         captchaKey = solver.solve()
         while captchaKey == None:
             captchaKey = solver.solve()
-
         self.session.headers = {
             'authority': 'discord.com',
             'accept': '*/*',
@@ -234,8 +164,6 @@ class Discord():
         token = self.createAccount(captchaKey, fingerprint)
         if token == None:
             return
-        
-        #Updating Headers
         self.session.headers.pop('x-track')
         self.session.headers['referer'] = 'https://discord.com/channels/@me'
         self.session.headers.update({
@@ -244,81 +172,59 @@ class Discord():
             'x-debug-options': 'bugReporterEnabled',
             'authorization': token
         })
-
-        if not self.isLocked():
-            total += 1
-            self.session.proxies = {
-                "http": None,
-                "https": None
-            }
-             
-            #Connect to websockets
-            ws = websocket.WebSocket()
-            ws.connect('wss://gateway.discord.gg/?encoding=json&v=9')
-            ws.send(json.dumps({"op": 2, "d": {"token": token, "capabilities": 4093, "properties": self.prop, "presence": {"status": "online", "since": 0, "activities": [], "afk": False}, "compress": False, "client_state": {"guild_versions": {}, "highest_last_message_id": "0", "read_state_version": 0, "user_guild_settings_version": -1, "user_settings_version": -1, "private_channels_version": "0", "api_code_version": 0}}}))
-            
-            added = ""
-            
-            #Set birth data + avatar if enabled
-            json_data = {
-                'date_of_birth': '2000-12-19',
-            }
-            if config['avatar']:
-                json_data['avatar']  = 'data:image/png;base64,' + base64.b64encode(open(os.path.join("input/image", random.choice([f for f in os.listdir("input/image") if f.endswith('.jpg') or f.endswith('.png')])), 'rb').read()).decode('utf-8')
-                added += "Avatar, "
-            response = self.session.patch('https://discord.com/api/v9/users/@me', json=json_data)
-            if  response.status_code == 200:
-                added += "BirthDate, "
-            else:
-                ws.close()
-                locked += 1
-                Output.error(f'Locked [{token[:30]}*************************]')
-                return
-
-            #HypeSquad
-            if config['hypesquad']:
-                response = self.session.post(
-                    'https://discord.com/api/v9/hypesquad/online',
-                    json={
-                        'house_id': random.randint(1,3),
-                    }
-                )
-                if response.status_code == 204:
-                    added += "Hypesquad, "
-                else:
-                    ws.close()
-                    locked += 1
-                    Output.error(f'Locked [{token[:30]}*************************]')
-                    return
-
-            #Bio
-            if config['bio']:
-                bio = random.choice(open('input/bios.txt', 'r', encoding="utf-8").read().splitlines())
-
-                response = self.session.patch(
-                    'https://discord.com/api/v9/users/%40me/profile',
-                    json={
-                        'bio': bio,
-                    }
-                )
-                if response.status_code == 200:
-                    added += "Bio, "
-                else:
-                    ws.close()
-                    locked += 1
-                    Output.error(f'Locked [{token[:30]}*************************]')
-                    return
-            
-            unlocked += 1
-            open('tokens.txt', 'a').write(f'{token}\n')
-            Output.good(f'Unlocked [{token[:30]}*************************]')
-            ws.close()
-            Output.other(f'Humanized: {added}')
-            #self.friend_request(token)
-        else:
+        if self.isLocked():
             total += 1
             locked += 1
-            Output.error(f'Locked [{token[:30]}*************************]')
+            print(f"({Fore.RED}-{Style.RESET_ALL}) - Locked [{token[:30]}*************************]")
+            return
+        total += 1
+        self.session.proxies = {"http": None, "https": None}
+        ws = websocket.WebSocket()
+        ws.connect('wss://gateway.discord.gg/?encoding=json&v=9')
+        ws.send(json.dumps({
+            "op": 2,
+            "d": {
+                "token": token,
+                "capabilities": 4093,
+                "properties": self.prop,
+                "presence": {
+                    "status": "online",
+                    "since": 0,
+                    "activities": [],
+                    "afk": False
+                },
+                "compress": False,
+                "client_state": {
+                    "guild_versions": {},
+                    "highest_last_message_id": "0",
+                    "read_state_version": 0,
+                    "user_guild_settings_version": -1,
+                    "user_settings_version": -1,
+                    "private_channels_version": "0",
+                    "api_code_version": 0
+                }
+            }
+        }))
+        added = ""
+        json_data = {'date_of_birth': '2000-12-19',}
+        json_data['avatar']  = 'data:image/png;base64,' + base64.b64encode(open(os.path.join("input/image", random.choice([f for f in os.listdir("input/image") if f.endswith('.jpg') or f.endswith('.png')])), 'rb').read()).decode('utf-8')    
+        added += "Avatar, "
+        response = self.session.patch('https://discord.com/api/v9/users/@me', json=json_data)
+        if response.status_code == 200:
+            added += "BirthDate, "
+        response = self.session.post('https://discord.com/api/v9/hypesquad/online', json={'house_id': random.randint(1, 3)})
+        if response.status_code == 204:
+            added += "Hypesquad, "
+        bio = random.choice(open('input/bios.txt', 'r', encoding="utf-8").read().splitlines())
+        response = self.session.patch('https://discord.com/api/v9/users/%40me/profile', json={'bio': bio})
+        if response.status_code == 200:
+            added += "Bio "
+        unlocked += 1
+        open('tokens.txt', 'a').write(f'{token}\n')
+        ws.close()
+        print(f"({Fore.GREEN}+{Style.RESET_ALL}) - Unlocked [{token[:30]}*************************]")
+        print(f"({Fore.MAGENTA}~{Style.RESET_ALL}) - Humanized: {added}")
+
 
 def setupBrowser() -> None:
     browser = Browser()
@@ -334,11 +240,11 @@ def generate() -> None:
             discord = Discord()
             discord.generate()
         except Exception as e:
-            Output.error(str(e))
+            #print(f"({Fore.RED}-{Style.RESET_ALL}) - Error [{e}")
+            pass
 
 if __name__ == "__main__":
-    Console.clear()
-    print(Colorate.Diagonal(Colors.purple_to_red, Center.XCenter(Ascii.get('Balls  Gen', AsciiType.DOH))))
+    os.system('cls')
     for _ in range(int(input(f'({Fore.LIGHTMAGENTA_EX}~{Fore.RESET}) - Browsers → '))): thread = threading.Thread(target=setupBrowser); thread.start(); threadList.append(thread)
     for t in threadList: t.join()
     for i in range(int(input(f'({Fore.LIGHTMAGENTA_EX}~{Fore.RESET}) - Threads → '))):
